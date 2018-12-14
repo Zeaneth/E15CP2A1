22082311ee5a8519aed5168d9a7953cd87e32182
Zamiz <zamiztobar@gmail.com>
2018-12-13 21:19:25 -0300

    add everything till navbar public histories view

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,4 +1,4 @@
 class History < ApplicationRecord
   mount_uploader :picture, ImageUploader
-  belong_to :user
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :histories
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
 end
